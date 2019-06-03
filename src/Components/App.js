53e177f73cec9ca980b68de5ae8aaa55e1e12306
Ted Newton <tednewton@teds-mbp.home>
2019-06-03 20:22:43 +0100

    first commit, created basic functionality for adding and removing players

--- src/Components/App.js
+++ src/Components/App.js
@@ -5,15 +5,40 @@ import { suits, values } from "../utils";
 import Layout from "./Layout";
 import Deck from "./Deck";
 import Player from "./Player";
-import Button from "./Button";
+// import Button from "./Button";
 
 import { Footer } from "../Styles/Styled";
 
 class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			players: ['Ted', 'Penny'],
+			errorMessage: '',
+			playerIndex: ''
+		}
+	}
+	addPlayer = () => {
+		if(this.state.players.length < 6) {
+			const updatedPlayers = this.state.players;
+			updatedPlayers.push(`Player ${this.state.players.length + 1}`)
+			this.setState({players: updatedPlayers});
+		} else {
+			this.setState({errorMessage: 'The max number of players is 6'})
+		}
+	}
+	handleRemovePlayer = index => {
+		if(this.state.players.length > 2) {
+			const players = this.state.players;
+			players.splice(index, 1);
+			this.setState({players});
+		} else {
+			this.setState({errorMessage: 'The min number of players is 2'})
+		}
+	}
 	render() {
 		return (
 				<Layout>
-
 					<section>
 						<h1>Cards deck</h1>
 						<Deck suits={suits} values={values} />
@@ -23,12 +48,14 @@ class App extends Component {
 							<h1>Players</h1>
 						</header>
 						<section>
-							<Player name="Player 1" />
-							<Player name="Player 2" />
+							{this.state.players.map((name, index) => {
+								return <Player onDelete={this.handleRemovePlayer} name={name} index={index} key={index}/>
+							})}
 						</section>
 						<Footer>
-							<Button icon="🙋‍♀️">Add new player</Button>
-							<Button icon="🏆">Find the winner</Button>
+							<button onClick={this.addPlayer}>Add new player</button>
+							<button icon="🏆">Find the winner</button>
+							<p>{this.state.errorMessage}</p>
 						</Footer>
 					</section>
 
--- src/Components/Player.js
+++ src/Components/Player.js
@@ -1,34 +1,46 @@
-import React from "react";
+import React, { Component } from 'react';
 
 import Button from "./Button";
 
 import { Card, PlayerHand } from "../Styles/Styled";
 
-const Player = ({ name }) => (
-	<article>
-		<p>
-			{name}
-			<Button icon="✏️">Edit</Button>
-			<Button icon="🔥">Remove</Button>
-		</p>
-		<PlayerHand>
-				<Card suit="D" value="A" selected={true}>
-					A
-				</Card>
-				<Card suit="D" value="K">
-					K
-				</Card>
-				<Card suit="D" value="Q">
-					Q
-				</Card>
-				<Card suit="D" value="J">
-					J
-				</Card>
-				<Card suit="D" value="T">
-					T
-				</Card>
-		</PlayerHand>
-	</article>
-);
+class Player extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {}
+	}
+	handleDeletePlayer = () => {
+		console.log(this.props.index)
+		this.props.onDelete(this.props.index);
+	}
+	render() {
+		return (
+			<article>
+				<p>
+					{this.props.name}
+					<Button icon="✏️">Edit</Button>
+					<button onClick={this.handleDeletePlayer} icon="🔥">Remove</button>
+				</p>
+				<PlayerHand>
+						<Card suit="D" value="A" selected={true}>
+							A
+						</Card>
+						<Card suit="D" value="K">
+							K
+						</Card>
+						<Card suit="D" value="Q">
+							Q
+						</Card>
+						<Card suit="D" value="J">
+							J
+						</Card>
+						<Card suit="D" value="T">
+							T
+						</Card>
+				</PlayerHand>
+			</article>
+		)
+	}
+};
 
 export default Player;
